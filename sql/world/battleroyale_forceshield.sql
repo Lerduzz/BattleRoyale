2867365e52259a0f873e3109a2392701d4e1c063
Lerduzz <lerduzz@sking.cu>
2023-09-03 23:16:13 -0400

    AddOn para el centro del mapa no despawn.

--- sql/world/battleroyale_forceshield.sql
+++ sql/world/battleroyale_forceshield.sql
@@ -2,7 +2,8 @@ SET
 @Entry 		:= 500000,
 @DisplayID  := 8612, -- 7203
 @Name       := 'Zona Segura BR';
-DELETE FROM `gameobject_template` WHERE `entry` >= @Entry+0 AND `entry` <= @Entry+10;
+DELETE FROM `gameobject_template` WHERE `entry` >= @Entry+0 AND `entry` < @Entry+11;
+DELETE FROM `gameobject_template_addon` WHERE `entry` >= @Entry+0 AND `entry` < @Entry+11;
 INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`, `Data3`) VALUES (@Entry+0, @DisplayID, @Name, 4.5,  5, 1);
 INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`, `Data3`) VALUES (@Entry+1, @DisplayID, @Name, 4.0,  5, 1);
 INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`, `Data3`) VALUES (@Entry+2, @DisplayID, @Name, 3.5,  5, 1);
@@ -14,4 +15,6 @@ INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`,
 INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`, `Data3`) VALUES (@Entry+8, @DisplayID, @Name, 0.5,  5, 1);
 INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`, `Data3`) VALUES (@Entry+9, @DisplayID, @Name, 0.25, 5, 1);
 
+-- Centro de la partida: se puede intentar poner un AZ de luz.
 INSERT INTO `gameobject_template` (`entry`, `displayId`, `name`, `size`, `type`) VALUES (@Entry+10, 0, "Centro Invisible BR", 1.0, 5);
+INSERT INTO `gameobject_template_addon` (`entry`, `flags`) VALUES (@Entry+10, 32);
