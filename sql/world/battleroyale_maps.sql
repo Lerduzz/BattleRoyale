8508e43c41facffd962a3d49d913d9bdc68166ac
Lerduzz <lerduzz@sking.cu>
2023-09-13 18:21:44 -0400

    Tabla de las locaciones de los cofres.

--- sql/world/battleroyale_maps.sql
+++ sql/world/battleroyale_maps.sql
@@ -1,15 +1,25 @@
 DROP TABLE IF EXISTS `battleroyale_maps`;
 CREATE TABLE `battleroyale_maps` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `map_id` INT UNSIGNED NOT NULL DEFAULT 0, 
-    `map_name` VARCHAR(255) NOT NULL, 
-    `center_x` FLOAT NOT NULL DEFAULT 0, 
-    `center_y` FLOAT NOT NULL DEFAULT 0, 
-    `center_z` FLOAT NOT NULL DEFAULT 0, 
-    `center_o` FLOAT NOT NULL DEFAULT 0, 
-    `ship_x` FLOAT NOT NULL DEFAULT 0, 
-    `ship_y` FLOAT NOT NULL DEFAULT 0, 
-    `ship_z` FLOAT NOT NULL DEFAULT 0, 
-    `ship_o` FLOAT NOT NULL DEFAULT 0, 
+    `id` INT NOT NULL AUTO_INCREMENT, 
+    `map_id` INT UNSIGNED NOT NULL DEFAULT 0,
+    `map_name` VARCHAR(255) NOT NULL,
+    `center_x` FLOAT NOT NULL DEFAULT 0,
+    `center_y` FLOAT NOT NULL DEFAULT 0,
+    `center_z` FLOAT NOT NULL DEFAULT 0,
+    `center_o` FLOAT NOT NULL DEFAULT 0,
+    `ship_x` FLOAT NOT NULL DEFAULT 0,
+    `ship_y` FLOAT NOT NULL DEFAULT 0,
+    `ship_z` FLOAT NOT NULL DEFAULT 0,
+    `ship_o` FLOAT NOT NULL DEFAULT 0,
 PRIMARY KEY (`id`));
 
+DROP TABLE IF EXISTS `battleroyale_maps_spawns`;
+CREATE TABLE `battleroyale_maps_spawns` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `zone` INT NOT NULL DEFAULT 0,
+    `map` INT NOT NULL DEFAULT 0,
+    `pos_x` FLOAT NOT NULL DEFAULT 0,
+    `pos_y` FLOAT NOT NULL DEFAULT 0,
+    `pos_z` FLOAT NOT NULL DEFAULT 0,
+    `pos_o` FLOAT NOT NULL DEFAULT 0,
+PRIMARY KEY (`id`));
