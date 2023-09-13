d75d5185dc101e9007334d4d0ba8a9e1e1cb36a7
Lerduzz <lerduzz@sking.cu>
2023-09-13 18:15:32 -0400

    Crear tabla para los mapas.

---
+++ sql/world/battleroyale_maps.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `battleroyale_maps` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    `map_id` INT UNSIGNED NOT NULL DEFAULT 0, 
+    `map_name` VARCHAR(255) NOT NULL, 
+    `center_x` FLOAT NOT NULL DEFAULT 0, 
+    `center_y` FLOAT NOT NULL DEFAULT 0, 
+    `center_z` FLOAT NOT NULL DEFAULT 0, 
+    `center_o` FLOAT NOT NULL DEFAULT 0, 
+    `ship_x` FLOAT NOT NULL DEFAULT 0, 
+    `ship_y` FLOAT NOT NULL DEFAULT 0, 
+    `ship_z` FLOAT NOT NULL DEFAULT 0, 
+    `ship_o` FLOAT NOT NULL DEFAULT 0, 
+PRIMARY KEY (`id`));
+
