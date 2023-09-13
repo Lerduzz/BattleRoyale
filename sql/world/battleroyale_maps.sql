a61bc9b6d567e24a310f088d6a2b3ffed5a76581
Lerduzz <lerduzz@sking.cu>
2023-09-13 18:36:08 -0400

    Agregados los mapas que ya estaban medidos.

--- sql/world/battleroyale_maps.sql
+++ sql/world/battleroyale_maps.sql
@@ -23,3 +23,9 @@ CREATE TABLE `battleroyale_maps_spawns` (
     `pos_z` FLOAT NOT NULL DEFAULT 0,
     `pos_o` FLOAT NOT NULL DEFAULT 0,
 PRIMARY KEY (`id`));
+
+INSERT  INTO `battleroyale_maps`(`id`,`map_id`,`map_name`,`center_x`,`center_y`,`center_z`,`center_o`,`ship_x`,`ship_y`,`ship_z`,`ship_o`) VALUES
+(1, 1, 'Kalimdor: Hyjal', 5261.58, -2164.18, 1259.48, 0, 2967.58, -2164.18, 1556.48, -1.5708),
+(2, 0, 'Reinos del Este: Isla Jaguero', -14614.6, -313.263, 0.000001, 0, -12320.6, -313.263, 297, 1.5708),
+(3, 1, 'Kalimdor: Las Charcas del Olvido', 157.996, -1948.09, 87.3871, 0, -2137.58, -1948.09, 394.387, -1.5708),
+(4, 1, 'Kalimdor: Ruinas de Solarsal', -4695.31, 3411.21, 7.05051, 0, -2401.31, 3411.21, 304.051, 1.5708);
