035b686c232ce3f2211b701dc84dc34bbd9e8005
Lerduzz <lerduzz@sking.cu>
2023-09-18 12:15:13 -0400

    Columna para el motivo.

--- sql/world/battleroyale_blacklist.sql
+++ sql/world/battleroyale_blacklist.sql
@@ -1,3 +1,4 @@
-CREATE TABLE `battleroyale_blacklist` ( 
-    `guid` INT UNSIGNED NOT NULL, 
+CREATE TABLE IF NOT EXISTS `battleroyale_blacklist` ( 
+    `guid` INT UNSIGNED NOT NULL,
+    `reason` VARCHAR(255) NOT NULL DEFAULT 'Sin motivo.',
 PRIMARY KEY (`guid`) );
