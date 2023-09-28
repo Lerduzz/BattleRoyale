1e0e770a8e6c104bea37c4db32eab245de88840a
Lerduzz <lerduzz@sking.cu>
2023-09-28 05:36:29 -0400

    DB: Editando el fichero SQL de los item.

--- sql/world/battleroyale_items.sql
+++ sql/world/battleroyale_items.sql
@@ -1,25 +1,9 @@
 SET
-@Entry 		   := 17,
-@Class         := 4,
-@Name          := 'Alas de Battle Royale',
-@DisplayID     := 7016,
 @Quality       := 6,
-@ItemLevel     := 1,
-@InventoryType := 4,
-@Delay         := 0,
-@Spell         := 18282,
-@Cooldown       := 2500,
-@Bonding       := 1,
-@Description   := 'Activa y desactiva las Alas Mágicas del modo Battle Royale. ¡A que mola, Eh!',
-@Material      := 6,
-@Sheath        := 3,
-@Duration      := 900,
-@Flags         := 128,
-@FlagsCustom   := 1,
-@Script        := 'BattleRoyaleItem';
+@Duration      := 900;
 
-DELETE FROM `item_template` WHERE `entry` = @Entry;
-DELETE FROM `item_template_locale` WHERE `ID` = @Entry;
+DELETE FROM `item_template` WHERE `entry` IN (17);
+DELETE FROM `item_template_locale` WHERE `ID` IN (17);
 
 INSERT INTO `item_template` (`entry`, `class`, `name`, `displayid`, `Quality`, `ItemLevel`, `InventoryType`, `delay`, `spellid_1`, `spellcooldown_1`, `bonding`, `description`, `Material`, `sheath`, `duration`, `Flags`, `flagsCustom`, `ScriptName`) VALUES
-(@Entry, @Class, @Name, @DisplayID, @Quality, @ItemLevel, @InventoryType, @Delay, @Spell, @Cooldown, @Bonding, @Description, @Material, @Sheath, @Duration, @Flags, @FlagsCustom, @Script);
+(17, 4, 'Camisa de Battle Royale', 7016, @Quality, 1, 4, 0, 18282, 1500, 1, 'Uso: Activa y desactiva las Alas Mágicas del modo Battle Royale cada 1.5 segundos.', 6, 3, @Duration, 128, 1, 'BattleRoyaleItem');
