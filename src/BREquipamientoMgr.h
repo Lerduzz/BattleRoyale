a8244f7b360b0c0827dd70f82c7c2033582d3ebe
Lerduzz <lerduzz@sking.cu>
2023-09-28 03:31:26 -0400

    Creada funcion generica de dar y quitar objetos.

--- src/BREquipamientoMgr.h
+++ src/BREquipamientoMgr.h
@@ -6,6 +6,7 @@
 enum BR_Equipamiento
 {
     EQUIPAMIENTO_CAMISA_ALAS                  = 17,
+    EQUIPAMIENTO_PIERNAS_BASE                 = 20902,
 };
 
 class BREquipamientoMgr
@@ -22,19 +23,30 @@ public:
 
     bool EntregarAlas(Player* player)
     {
-        QuitarAlas(player);
-        return player->StoreNewItemInBestSlots(EQUIPAMIENTO_CAMISA_ALAS, 1);
+        return DarEquipamiento(player, EQUIPAMIENTO_CAMISA_ALAS);
     };
 
     bool QuitarAlas(Player* player)
     {
-        uint32 count = player->GetItemCount(EQUIPAMIENTO_CAMISA_ALAS, true);
+        return QuitarEquipamiento(player, EQUIPAMIENTO_CAMISA_ALAS);
+    };
+
+private:
+    bool QuitarEquipamiento(Player* player, BR_Equipamiento item)
+    {
+        uint32 count = player->GetItemCount(item, true);
         if (count > 0)
         {
-            player->DestroyItemCount(EQUIPAMIENTO_CAMISA_ALAS, count, true);
+            player->DestroyItemCount(item, count, true);
             return true;
         }
         return false;
+    };
+
+    bool DarEquipamiento(Player* player, BR_Equipamiento item)
+    {
+        QuitarEquipamiento(player, item);
+        return player->StoreNewItemInBestSlots(item, 1);
     };
 
 };
