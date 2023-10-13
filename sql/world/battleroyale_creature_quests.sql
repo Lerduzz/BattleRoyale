3287d58ce68c09ad42e777ea2d1850490a8a41a5
Lerduzz <lerduzz@sking.cu>
2023-10-12 22:22:10 -0400

    Hacer que vuele y se vea de lejos.

--- sql/world/battleroyale_creature_quests.sql
+++ sql/world/battleroyale_creature_quests.sql
@@ -126,3 +126,7 @@ SET
 @Scale4     := 0.5;
 INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `AiName`, `ScriptName`, `HealthModifier`, `ManaModifier`) VALUES
 (@Entry - 1, @Model4, @Name4, @Title4, '', @GossipMenu, @Level4, @Level4, @Faction4, @NPCFlag4, 1, 1.14286, @Scale4, 3, 8, 2+4+256+512+4096+131072+2147483648, 2, 4+16, 1, 256+16777216, '', '', 178.5, 158.7);
+DELETE FROM `creature_template_addon` WHERE `entry` = @Entry - 1;
+INSERT INTO `creature_template_addon` (`entry`, `visibilityDistanceType`) VALUES (@Entry - 1, 5);
+DELETE FROM `creature_template_movement` WHERE `CreatureId` = @Entry - 1;
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES (@Entry - 1, 1, 0, 1, 0, 1, 1, 60000); 
