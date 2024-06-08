93b33061d72c2277fba41a5692129bd409868104
Lerduzz <skxawngcu@gmail.com>
2024-06-08 13:37:39 -0600

    Creado el enum para los nuevos estados del evento.

--- src/BRConstantes.h
+++ src/BRConstantes.h
@@ -3,6 +3,20 @@
 
 #include "Player.h"
 
+enum BREstado
+{
+    ESTADO_BR_SIN_SUFICIENTES_JUGADORES = 0,    // Esperando a que se llene la cola o el inicio programado (30min).
+    ESTADO_BR_INVITANDO_JUGADORES,              // Invitando jugadores de la cola hasta llenar el evento (30seg).
+    ESTADO_BR_ESPERANDO_JUGADORES,              // Esperando que los jugadores terminen de aceptar el llamado (15seg).
+    ESTADO_BR_NAVE_EN_ESPERA,                   // Esperando que los jugadores salgan de pantalla de carga y compren armas (30seg).
+    ESTADO_BR_NAVE_EN_MOVIMIENTO,               // La nave avanza hacia la zona de la batalla (30seg).
+    ESTADO_BR_NAVE_POR_DESAPARECER,             // La nave esta girando para desaparecer (15seg).
+    ESTADO_BR_ZONA_EN_ESPERA,                   // BATALLA EN CURSO: Esperando para reducir la zona (60s) x5.
+    ESTADO_BR_ZONA_EN_REDUCCION,                // BATALLA EN CURSO: Esperando para reducir la zona (30s) x5.
+    ESTADO_BR_ZONA_DESAPARECIDA,                // BATALLA EN CURSO: Todos reciben daño de zona hasta que quede solo 1 con vida (~20seg).
+    ESTADO_BR_BATALLA_TERMINADA,                // Batalla terminada, esperando para cerrar el evento (10seg).
+}
+
 enum BR_EstadosEvento
 {
     ESTADO_NO_HAY_SUFICIENTES_JUGADORES = 0,
