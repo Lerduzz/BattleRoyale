02ecf30e78b86075c9ac10abc5663dd8f9d70e23
Lerduzz <skxawngcu@gmail.com>
2024-06-13 10:09:39 -0600

    Creando la clase que se encargara de manejar todo el evento.

---
+++ src/mgr/EventoMgr.cpp
@@ -0,0 +1,74 @@
+#include "EventoMgr.h"
+
+EventoMgr::EventoMgr()
+{
+    LimpiarEvento();
+}
+
+EventoMgr::~EventoMgr()
+{
+    LimpiarEvento();
+}
+
+bool EventoMgr::PuedeEntrarCola(Player *player)
+{
+    if (!player)
+    {
+        LOG_ERROR("event.br", "[BattleRoyale] EventoMgr::PuedeEntrarCola(Player *player): El jugador no exixte.");
+        return false;
+    }
+    if (player->getLevel() != 80)
+    {
+        // TODO: sBRChatMgr->AnunciarMensajeEntrada(player, MENSAJE_ERROR_NIVEL);
+        return false;
+    }
+    if (player->getClass() == CLASS_DEATH_KNIGHT && player->GetMapId() == 609)
+    {
+        // TODO: sBRChatMgr->AnunciarMensajeEntrada(player, MENSAJE_ERROR_DK_INICIO);
+        return false;
+    }
+    if (EstaEnCola(player))
+    {
+        // sBRChatMgr->AnunciarMensajeEntrada(player, MENSAJE_ERROR_EN_COLA);
+        return false;
+    }
+    if (EstaInvitado(player))
+    {
+        // sBRChatMgr->AnunciarMensajeEntrada(player, MENSAJE_ERROR_INVITADO);
+        return false;
+    }
+    if (EstaEnEvento(player))
+    {
+        // sBRChatMgr->AnunciarMensajeEntrada(player, MENSAJE_ERROR_EN_EVENTO);
+        return false;
+    }
+    return true;
+}
+
+bool EventoMgr::EstaEnCola(Player *player)
+{
+    return false;
+}
+
+bool EventoMgr::EstaInvitado(Player *player)
+{
+    return false;
+}
+
+bool EventoMgr::EstaEnEvento(Player *player)
+{
+    return false;
+}
+
+void EventoMgr::JugadorEntrando(Player *player)
+{
+    if (!PuedeEntrarCola(player))
+        return;
+    // TODO: Agregar a la cola, llamar al evento o iniciar ronda nueva segun estado actual.
+    LOG_ERROR("event.br", "[BattleRoyale] TODO: EventoMgr::JugadorEntrando(Player *player): El jugador cumple los requisitos para entrar en cola.");
+}
+
+void EventoMgr::LimpiarEvento()
+{
+    estado = BR_ESTADO_SIN_SUFICIENTES_JUGADORES;
+}
---
+++ src/mgr/EventoMgr.h
@@ -0,0 +1,77 @@
+#ifndef SC_BR_ENTIDAD_MGR_H
+#define SC_BR_ENTIDAD_MGR_H
+
+#include "Constantes.h"
+
+class EventoMgr
+{
+    EventoMgr();
+    ~EventoMgr();
+
+public:
+    static EventoMgr *instance()
+    {
+        static EventoMgr *instance = new EventoMgr();
+        return instance;
+    }
+
+    /**
+     * @brief Determina si un jugador cumple las condiciones para entrar en la cola del evento.
+     *
+     * @param player El jugador que esta intentando unirse a la cola.
+     * @return true
+     * @return false
+     */
+    bool PuedeEntrarCola(Player *player);
+
+    /**
+     * @brief Determina si el jugador se encuentra en la cola para el evento.
+     *
+     * @param player El jugador que se quiere comprobar.
+     * @return true
+     * @return false
+     */
+    bool EstaEnCola(Player *player);
+
+    /**
+     * @brief Determina si el jugador esta invitado al evento.
+     *
+     * @param player El jugador que se quiere comprobar.
+     * @return true
+     * @return false
+     */
+    bool EstaInvitado(Player *player);
+
+    /**
+     * @brief Determina si el jugador se encuentra dentro del evento.
+     *
+     * @param player El jugador que se quiere comprobar.
+     * @return true
+     * @return false
+     */
+    bool EstaEnEvento(Player *player);
+
+    /**
+     * @brief Gestiona al jugador que quiere entrar al evento.
+     *
+     * @param player EL jugador que quiere entrar al evento.
+     */
+    void JugadorEntrando(Player *player);
+
+private:
+    /**
+     * @brief Establece todas las variables del evento a su estado inicial.
+     *
+     */
+    void LimpiarEvento();
+
+    /**
+     * @brief Almacena el estado actual en el que se encuentra el evento.
+     *
+     */
+    BREstado estado;
+};
+
+#define sEventoMgr EventoMgr::instance()
+
+#endif
