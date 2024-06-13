f0ea127e29fb61f240582194681274c7b60d1795
Lerduzz <skxawngcu@gmail.com>
2024-06-13 16:08:48 -0600

    Mover EntidadMgr (Parte # 3).

--- src/core/EntidadMgr.h
+++ src/core/EntidadMgr.h
@@ -25,85 +25,14 @@ public:
     bool InvocarNave(uint32 mapID, Position pos);
     bool EncenderNave();
     bool EstaEnLaNave(Player *player);
-    bool InvocarZona(uint32 mapID, Position pos)
-    {
-        Map *map = sMapMgr->CreateBaseMap(mapID);
-        if (map)
-        {
-            DesaparecerZona();
-            float x = pos.GetPositionX();
-            float y = pos.GetPositionY();
-            float z = pos.GetPositionZ();
-            float o = pos.GetOrientation();
-            CreatureTemplate const *cinfo = sObjectMgr->GetCreatureTemplate(12999);
-            if (!cinfo)
-                return false;
-            npc_Centro = new Creature(true);
-            if (!npc_Centro->Create(map->GenerateLowGuid<HighGuid::Unit>(), map, BR_VISIBILIDAD_EVENTO, 12999, 0, x, y, z, o))
-            {
-                delete npc_Centro;
-                npc_Centro = nullptr;
-                return false;
-            }
-            npc_Centro->SetVisibilityDistanceOverride(VisibilityDistanceType::Infinite);
-            map->AddToMap(npc_Centro);
-            npc_Centro->AddAura(63894, npc_Centro);
-            npc_Centro->SetObjectScale(15.2f);
-            if (!(npc_Guardian = npc_Centro->SummonCreature(BR_CRIATURA_DRAGON_GUARDIAN, x, y, z + 330.0f, o, TEMPSUMMON_MANUAL_DESPAWN)))
-            {
-                delete npc_Guardian;
-                npc_Guardian = nullptr;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    bool InvocarCofre(Position pos)
-    {
-        if (EstaLaZonaActiva())
-        {
-            if (npc_Centro->SummonGameObject(BR_OBJETO_COFRE, pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ(), pos.GetOrientation(), 0, 0, 0, 0, 60))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool ActualizarZona(float &scale)
-    {
-        if (EstaLaZonaActiva())
-        {
-            if (scale < 0.2f)
-            {
-                npc_Centro->CleanupsBeforeDelete();
-                delete npc_Centro;
-                npc_Centro = nullptr;
-            }
-            else
-            {
-                npc_Centro->SetObjectScale(scale);
-                scale -= 0.01f;
-            }
-        }
-        return true;
-    }
-
-    float DistanciaDelCentro(Player *player) { return EstaLaZonaActiva() ? player->GetExactDist(npc_Centro) : 0.0f; }; // TODO: GetDist
-    bool EstaLaZonaActiva() { return npc_Centro ? true : false; };
-
-    bool HayNave() { return obj_Nave ? true : false; };
-    bool HayGuardian() { return npc_Guardian ? true : false; };
-
-    bool HechizoGuardian(uint32 spell, Player *player)
-    {
-        if (HayGuardian() && player && spell > 0)
-        {
-            return npc_Guardian->CastSpell(player, spell, true) == SPELL_CAST_OK;
-        }
-        return false;
-    }
+    bool InvocarZona(uint32 mapID, Position pos);
+    bool InvocarCofre(Position pos);
+    bool ActualizarZona(float &scale);
+    bool HechizoGuardian(uint32 spell, Player *player);
+    inline float DistanciaDelCentro(Player *player) { return EstaLaZonaActiva() ? player->GetExactDist(npc_Centro) : 0.0f; };
+    inline bool EstaLaZonaActiva() { return npc_Centro ? true : false; };
+    inline bool HayNave() { return obj_Nave ? true : false; };
+    inline bool HayGuardian() { return npc_Guardian ? true : false; };
 
 private:
     GameObject *obj_Nave;
